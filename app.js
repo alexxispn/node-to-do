20e7583a0e7254e8dd431570770c72b0f1df2f30
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2022-10-27 22:19:13 +0100

    add function of create task and list tasks

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
-import {pause, showMenu} from "./helpers/messages.js";
-import {inquirerMenu} from "./helpers/inquirer.js";
-
+import {inquirerMenu, pause, readInput} from "./helpers/inquirer.js";
+import Tasks from "./models/tasks.js";
 
 const main = async () => {
     let opt = "";
+    const tasks = new Tasks();
     do {
-        opt = await showMenu();
-        console.log({opt});
-        if (opt !== "0") await pause();
+        opt = await inquirerMenu();
+        switch (opt) {
+            case "1":
+                const description = await readInput("Description: ");
+                tasks.createTask(description);
+                break;
+            case "2":
+                console.log(tasks._list);
+                break;
+        }
+        await pause();
     } while (opt !== "0");
-};
+}
 
 main();
 
